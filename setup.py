b9ab7265c068a89bf3b4ae43897fb8412e79fce6
Aman Kothari <95128314+Drag0nSlay@users.noreply.github.com>
2026-06-04 18:31:07 +0530

    Bump version from 2.2.1 to 2.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="leakhunt",
-    version="2.2.1",
+    version="2.2.2",
     description="Independent secret discovery tool for URLs and local files",
     packages=find_packages(),
     include_package_data=True,
